3c871e68738f983db199e602497c6e83a776f88d
MANI <chandramanigupta2003@gmail.com>
2024-08-22 18:20:43 +0530

    first commit

---
+++ pr/src/components/Quiz/Quiz.jsx
@@ -0,0 +1,20 @@
+import "./Quiz.css";
+const Quiz = () => {
+  return (
+    <div className="container">
+      <h1 className="">Quiz App</h1>
+      <hr />
+      <h2>Which device is required for the internet connection ?</h2>
+      <ul>
+        <li>Modem</li>
+        <li>Router</li>
+        <li>LAN Cable</li>
+        <li>Pen Drive</li>
+      </ul>
+      <button>Next</button>
+      <div className="index">1 of 5 question</div>
+    </div>
+  );
+};
+
+export default Quiz;
