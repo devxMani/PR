32ef93b809e33e1fcb1d2f0621bfa5a357c13e76
MANI <chandramanigupta2003@gmail.com>
2024-08-22 21:45:41 +0530

    commit

--- pr/src/components/Quiz/Quiz.jsx
+++ pr/src/components/Quiz/Quiz.jsx
@@ -5,7 +5,17 @@ const Quiz = () => {
   let [index, setIndex] = useState(0);
   let [question, setQuestion] = useState(data[index]);
 
+  let [lock, setLock] = useState(false);
+
   const checkAns = (e, ans) => {
+    if (lock === false) {
+      e.target.classList.add("correct");
+      setLock(true);
+    } else {
+      e.target.classList.remove("correct");
+      setLock(true);
+    }
+
     if (question.ans === ans) {
       e.target.classList.add("correct");
     } else {
@@ -21,10 +31,34 @@ const Quiz = () => {
         {index + 1}. {question.question}
       </h2>
       <ul>
-        <li onClick={(e) => {checkAns(e,1)}}>{question.option1}</li>
-        <li onClick={(e) => {checkAns(e,2)}}>{question.option2}</li>
-        <li onClick={(e) => {checkAns(e,3)}}>{question.option3}</li>
-        <li onClick={(e) => {checkAns(e,4)}}>{question.option4}</li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 1);
+          }}
+        >
+          {question.option1}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 2);
+          }}
+        >
+          {question.option2}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 3);
+          }}
+        >
+          {question.option3}
+        </li>
+        <li
+          onClick={(e) => {
+            checkAns(e, 4);
+          }}
+        >
+          {question.option4}
+        </li>
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 question</div>
