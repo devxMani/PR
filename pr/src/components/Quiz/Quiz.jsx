629bb1fa9b240eb780bac2205dea945b8ab9517f
MANI <chandramanigupta2003@gmail.com>
2024-08-22 21:42:03 +0530

    commit

--- pr/src/components/Quiz/Quiz.jsx
+++ pr/src/components/Quiz/Quiz.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+import { data } from "../../assets/data";
 import "./Quiz.css";
 const Quiz = () => {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+
+  const checkAns = (e, ans) => {
+    if (question.ans === ans) {
+      e.target.classList.add("correct");
+    } else {
+      e.target.classList.add("incorrect");
+    }
+  };
+
   return (
     <div className="container">
       <h1 className="">Quiz App</h1>
       <hr />
-      <h2>1. Which device is required for the internet connection ?</h2>
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
       <ul>
-        <li>Modem</li>
-        <li>Router</li>
-        <li>LAN Cable</li>
-        <li>Pen Drive</li>
+        <li onClick={(e) => {checkAns(e,1)}}>{question.option1}</li>
+        <li onClick={(e) => {checkAns(e,2)}}>{question.option2}</li>
+        <li onClick={(e) => {checkAns(e,3)}}>{question.option3}</li>
+        <li onClick={(e) => {checkAns(e,4)}}>{question.option4}</li>
       </ul>
       <button>Next</button>
       <div className="index">1 of 5 question</div>
